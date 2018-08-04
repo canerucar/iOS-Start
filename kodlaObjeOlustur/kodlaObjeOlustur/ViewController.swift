091849f6150a8f1c8151acc812a07f6c75953a5d
Caner Uçar <canerucar0@gmail.com>
2018-08-04 16:38:37 +0300

    kodla obje oluşturmak
    sürükle bırak yerine bu sefer kodla objeler oluşturduk. zaman zaman herşeyi tasarım üstünden değil kodlara girerekde değişiklik yapacağımız anlar olacaktır

---
+++ kodlaObjeOlustur/kodlaObjeOlustur/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  kodlaObjeOlustur
+//
+//  Created by Caner Uçar on 4.08.2018.
+//  Copyright © 2018 Caner Uçar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+
+    let myLabel = UILabel()
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        //sadece main.storyboarddan sürükle bırakla değil bu şekilde kod ile de bir şeyler oluşturabiliriz
+        
+        let width = view.frame.size.width
+        let height = view.frame.size.height
+        
+         //Label
+        myLabel.text = "Breaking Bad?"
+        myLabel.textAlignment = .center
+        myLabel.frame = CGRect(x: width * 0.1, y: height * 0.3, width:
+            width * 0.8, height: 50)
+        view.addSubview(myLabel)
+        
+        
+        //button
+        let myButton = UIButton()
+        myButton.frame = CGRect(x: width*0.2, y: height*0.5, width: width*0.6, height: 50)
+        myButton.setTitle("Güncelle", for: UIControlState.normal)
+        myButton.setTitleColor(UIColor.blue, for: UIControlState.normal)
+        view.addSubview(myButton)
+        
+        
+        myButton.addTarget(self, action: #selector(ViewController.myAction) , for: UIControlEvents.touchUpInside)
+        
+        }
+    
+        @objc func myAction() {
+        
+        myLabel.text = "Better Call Saul"
+        
+        }
+        
+        
+        
+        }
+
+
+
