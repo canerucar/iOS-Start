230ddbd5e5ae7abacb676ed8a2dbb2735db1fdce
Caner Uçar <canerucar0@gmail.com>
2018-08-17 18:35:39 +0300

    instagram Clone Parse (Notification)

---
+++ instagramCloneParse/Insta Clone Parse/AppDelegate.swift
@@ -0,0 +1,106 @@
+//
+//  AppDelegate.swift
+//  Insta Clone Parse
+//
+//  Created by Caner Uçar on 17/08/2018.
+//  Copyright © 2017 Caner Uçar. All rights reserved.
+//
+
+import UIKit
+import Parse
+import OneSignal
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+         let onesignalInitSettings = [kOSSettingsKeyAutoPrompt: false]
+        
+        //ONESIGNAL
+        
+        
+        OneSignal.initWithLaunchOptions(launchOptions,
+                                        appId: "a920b83d-1496-4797-8f13-a866263afd64",
+                                        handleNotificationAction: nil,
+                                        settings: onesignalInitSettings)
+        
+        OneSignal.inFocusDisplayType = OSNotificationDisplayType.notification;
+        
+        // Recommend moving the below line to prompt for push after informing the user about
+        //   how your app will use them.
+        OneSignal.promptForPushNotifications(userResponse: { accepted in
+            print("User accepted notifications: \(accepted)")
+        })
+        
+        // Sync hashed email if you have a login system or collect it.
+        //   Will be used to reach the user at the most optimal time of day.
+        // OneSignal.syncHashedEmail(userEmail)
+        
+        
+        rememberUser()
+        
+        let config = ParseClientConfiguration { (ParseMutableClientConfiguration) in
+            
+            ParseMutableClientConfiguration.applicationId = "11b144981119a15105fcd54afcb43b43e098d965"
+            ParseMutableClientConfiguration.clientKey = "38c866ef607792a126fa011dba786e0a29693646"
+            ParseMutableClientConfiguration.server = "http://ec2-52-88-249-139.us-west-2.compute.amazonaws.com:80/parse"
+            
+        }
+        
+        Parse.initialize(with: config)
+        
+        let defaultACL = PFACL()
+        
+        defaultACL.getPublicReadAccess = true
+        defaultACL.getPublicWriteAccess = true
+        
+        PFACL.setDefault(defaultACL, withAccessForCurrentUser: true)
+        
+        
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+
+    
+    func rememberUser() {
+        
+        let user : String? = UserDefaults.standard.string(forKey: "username")
+        
+        if user != nil {
+            
+            let board : UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+            
+            let tabBar = board.instantiateViewController(withIdentifier: "tabBar") as! UITabBarController
+            
+            window?.rootViewController = tabBar
+            
+        }
+        
+    }
+}
+
---
+++ instagramCloneParse/Insta Clone Parse/uploadVC.swift
@@ -0,0 +1,93 @@
+//
+//  SecondViewController.swift
+//  Insta Clone Parse
+//
+//  Created by Caner Uçar on 17/08/2018.
+//  Copyright © 2017 Caner Uçar. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class uploadVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var postImage: UIImageView!
+    @IBOutlet weak var postCommentText: UITextView!
+    @IBOutlet weak var postButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        let keyboardRecognizer = UITapGestureRecognizer(target: self, action: #selector(uploadVC.hideKeyboard))
+        self.view.addGestureRecognizer(keyboardRecognizer)
+        
+        postImage.isUserInteractionEnabled = true
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(uploadVC.choosePhoto))
+        postImage.addGestureRecognizer(gestureRecognizer)
+        
+        postButton.isEnabled = false
+        
+    }
+    
+    @objc func hideKeyboard() {
+        
+        self.view.endEditing(true)
+        
+    }
+    
+    @objc func choosePhoto() {
+        
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.sourceType = .photoLibrary
+        pickerController.allowsEditing = true
+        present(pickerController, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        postImage.image = info[UIImagePickerControllerEditedImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
+        postButton.isEnabled = true
+    }
+
+ 
+    @IBAction func postButtonClicked(_ sender: Any) {
+        
+        self.postButton.isEnabled = false
+        
+        let object = PFObject(className: "Posts")
+        
+        let data = UIImageJPEGRepresentation(postImage.image!, 0.5)
+        let pfImage = PFFile(name: "image.jpg", data: data!)
+        
+        object["postimage"] = pfImage
+        object["postcomment"] = postCommentText.text
+        object["postowner"] = PFUser.current()!.username!
+        
+        let uuid = UUID().uuidString
+        
+        object["postuuid"] = "\(uuid) \(PFUser.current()!.username!)"
+        
+        object.saveInBackground { (success, error) in
+            if error != nil {
+                let alert = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: UIAlertControllerStyle.alert)
+                let okButton = UIAlertAction(title: "OK", style: UIAlertActionStyle.cancel, handler: nil)
+                alert.addAction(okButton)
+                self.present(alert, animated: true, completion: nil)
+            } else {
+                self.postCommentText.text = ""
+                self.postImage.image = UIImage(named: "select.png")
+                self.tabBarController?.selectedIndex = 0
+                
+                NotificationCenter.default.post(name: NSNotification.Name(rawValue: "newPost"), object: nil)
+                
+            }
+        }
+        
+    }
+    
+
+}
+
