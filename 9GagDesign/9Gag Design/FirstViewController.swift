a3d70e5d9e07784fd1422af9b0cb7933b44524a9
Caner Uçar <canerucar0@gmail.com>
2018-08-18 00:47:53 +0300

    9GagDesign and Sketch Design

---
+++ 9GagDesign/9Gag Design/FirstViewController.swift
@@ -0,0 +1,34 @@
+//
+//  AppDelegate.swift
+//  9Gag Design
+//
+//  Created by Atil Samancioglu on 18/08/2018.
+//  Copyright © 2017 Caner Uçar. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        return cell
+    }
+
+}
+
---
+++ 9GagDesign/9Gag Design/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  9Gag Design
+//
+//  Created by Atil Samancioglu on 18/08/2018.
+//  Copyright © 2017 Caner Uçar. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
