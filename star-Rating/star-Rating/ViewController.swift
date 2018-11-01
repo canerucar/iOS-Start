c021d5a938c7d913ce6b463a608a4b0eee3da395
Caner Uçar <canerucar0@gmail.com>
2018-11-01 11:47:19 +0300

    Yıldızla Puanlama

---
+++ star-Rating/star-Rating/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  star-Rating
+//
+//  Created by Caner Uçar on 1.11.2018.
+//  Copyright © 2018 Caner Uçar. All rights reserved.
+//
+
+import UIKit
+import SwiftyStarRatingView
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var rateView: SwiftyStarRatingView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
