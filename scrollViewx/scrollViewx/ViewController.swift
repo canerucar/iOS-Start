fc00b782787c7a50539650311acf9b677bee692e
Caner Uçar <canerucar0@gmail.com>
2018-11-13 01:29:11 +0300

    scroll View

---
+++ scrollViewx/scrollViewx/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  scrollViewx
+//
+//  Created by Caner Uçar on 12.11.2018.
+//  Copyright © 2018 Caner Uçar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
