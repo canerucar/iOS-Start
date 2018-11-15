adab6463d2d1fb0ecd81661c4e3e1c404705c999
Caner Uçar <canerucar0@gmail.com>
2018-11-15 17:07:52 +0300

    Compact-Regular Nasıl Kullanılır?

---
+++ compact-regular/compact-regular/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  compact-regular
+//
+//  Created by Caner Uçar on 8.11.2018.
+//  Copyright © 2018 Caner Uçar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
